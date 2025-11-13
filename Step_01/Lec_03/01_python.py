204d57b5ce2068177ad961f9107c75e2d6125a39
OnkarGhadage <onkaryg@gmail.com>
2025-11-13 21:01:26 +0530

    Add basic data types of Python in 01_python.py

---
+++ Step_01/Lec_03/01_python.py
@@ -0,0 +1,29 @@
+### List
+List = [1,9,2,3,4]    # List
+
+List.append(5)
+List.insert(1,10) #insert(i,x)
+List.extend(List) #extend(list2)
+
+List.remove(10)
+List.pop(1)   #pop(i)
+List.pop() # Last item poped and returned # List.clear() # Full list -
+
+List.index(4) # Return 4 value index i.e. 3
+List.count(2) # Return count of x
+
+List.sort() # Sorts original List
+List.sort(reverse=True) # Sorts original List
+List.reverse() # Reverse the list
+
+### Tuple
+Tuple = (1,2,3,4)   # Can have duplicate values (Immutable)
+# Refer notes
+
+### Set
+Set = {1,2,3,4}     # Can not have the duplicate values (Only unique)
+# Refer notes
+
+### Dictionary
+Dict = {1:"Onkar", 2:"Yashawantrao", 3:"Ghadage"} # Key: Value pair
+# Refer notes
