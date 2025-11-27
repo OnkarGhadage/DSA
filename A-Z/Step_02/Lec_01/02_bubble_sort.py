3f3965f976d3e3f6dfc16522342b666286982a3f
Onkar Ghadage <onkaryg@gmail.com>
2025-11-27 09:08:47 +0530

    Optimized bubble sort code

--- A-Z/Step_02/Lec_01/02_bubble_sort.py
+++ A-Z/Step_02/Lec_01/02_bubble_sort.py
@@ -2,9 +2,13 @@ class Solution:
     def bubble_sort(self, nums: list[int]) -> list:
         n = len(nums)
         for i in range(n-1):
+            swaped = False
             for j in range(n-i-1):
                 if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+                    swaped = True
+            if not swaped:
+                break
         return nums
 
 
