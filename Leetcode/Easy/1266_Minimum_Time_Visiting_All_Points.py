b9033f225c52a5ca4c49ffd5c12e2c63d9bdf863
OnkarGhadage <onkaryg@gmail.com>
2026-01-12 18:06:06 +0530

    Corrected syntax error

--- Leetcode/Easy/1266_Minimum_Time_Visiting_All_Points.py
+++ Leetcode/Easy/1266_Minimum_Time_Visiting_All_Points.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+    def minTimeToVisitAllPoints(self, points: list[list[int]]) -> int:
         total_time = 0
         for i in range(len(points) - 1):
             max_x = max(points[i][0], points[i+1][0])
