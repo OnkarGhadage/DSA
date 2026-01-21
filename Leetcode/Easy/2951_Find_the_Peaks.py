ac1f4ad65ab1fd3e24bc69ff5fd65b5a0d24f455
OnkarGhadage <onkaryg@gmail.com>
2026-01-21 09:40:16 +0530

    Add solution of leetcode problem 2951

---
+++ Leetcode/Easy/2951_Find_the_Peaks.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPeaks(self, mountain: list[int]) -> list[int]:
+        peaks = []
+        for i in range(1, len(mountain) - 1):
+            if mountain[i-1] < mountain[i] > mountain[i + 1]:
+                peaks.append(i)
+        return peaks
---
+++ Leetcode/extras.md
@@ -0,0 +1,4 @@
+# Extras leetcode problems
+
+### ⭐ 2951. Find the Peaks
+📙 **Leetcode** - [Problem statement](https://leetcode.com/problems/find-the-peaks/) → [**Solution**]()  
