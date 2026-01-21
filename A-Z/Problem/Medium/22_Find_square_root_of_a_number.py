027e8333b4f42e440a5875b5281e9b31b147218f
OnkarGhadage <onkaryg@gmail.com>
2026-01-21 09:35:22 +0530

    Add solution of TUF problem 'Find square root of a number'

---
+++ A-Z/Problem/Medium/22_Find_square_root_of_a_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def floorSqrt(self, n: int) -> int:
+        low, high = 0, n
+        ans = 0
+        while low <= high:
+            mid = (low + high) // 2
+            if (mid * mid) == n:
+                return mid
+            elif (mid * mid) > n:
+                high = mid - 1
+            else:
+                ans = mid
+                low = mid + 1
+        return ans
---
+++ A-Z/Step_04 (Binary Search)/02_BS_on_Answers.md
@@ -0,0 +1,6 @@
+# Binary Search
+
+## On Answers
+
+### ⭐ 1. Find square root of a number
+📘 **TUF** - [Problem statement](https://takeuforward.org/plus/dsa/problems/find-square-root-of-a-number) → [**Solution**](../Problem/Medium/22_Find_square_root_of_a_number.py)  
