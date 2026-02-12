7de4529dd49d2d0a52d18bcc5db8ef8183c103a3
OnkarGhadage <onkaryg@gmail.com>
2026-02-12 17:09:13 +0530

    Add solution of leetcode problem 1482

---
+++ Leetcode/Medium/1482_Minimum_Number_of_Days_to_Make_m_Bouquets.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minDays(self, bloomDay: list[int], m: int, k: int) -> int:
+        if len(bloomDay) < (m * k):
+            return -1
+
+        low, high = min(bloomDay), max(bloomDay)
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            possible_bouques = self.count(bloomDay, mid, k)
+
+            if possible_bouques >= m:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
+        
+    def count(self, bloomDay, day, k):
+        bouques = 0
+        streak = 0
+
+        for i in bloomDay:
+            if i <= day:
+                streak += 1
+                if streak == k:
+                    bouques += 1
+                    streak = 0
+            else:
+                streak = 0
+
+        return bouques
