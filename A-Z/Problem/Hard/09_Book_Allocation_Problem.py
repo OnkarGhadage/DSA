d913592b3ff3b8df74a62e864cee975e8d603f17
OnkarGhadage <onkaryg@gmail.com>
2026-02-26 17:29:02 +0530

    Add another solution of TUF problem 'Book Allocation Problem'
    Writen with better understanding

--- A-Z/Problem/Hard/09_Book_Allocation_Problem.py
+++ A-Z/Problem/Hard/09_Book_Allocation_Problem.py
@@ -1,29 +1,56 @@
+# class Solution:
+#     def findPages(self, nums, m):
+#         if len(nums) < m:
+#             return -1
+#         low, high = max(nums), sum(nums)
+
+#         while low <= high:
+#             mid = (low + high) // 2
+
+#             is_possible = self.cal(nums, mid, m)
+
+#             if is_possible:
+#                 high = mid - 1
+
+#             else:
+#                 low = mid + 1
+
+#         return low
+    
+#     def cal(self, nums, mid, m):
+#         students = 1
+#         sum = 0
+#         for i in nums:
+#             if sum + i > mid:
+#                 students += 1
+#                 sum = i
+#             else:
+#                 sum += i
+#         return students <= m
+
+### ---Another solution--- ###
+### Writed with better understanding ###
 class Solution:
     def findPages(self, nums, m):
         if len(nums) < m:
             return -1
         low, high = max(nums), sum(nums)
-
         while low <= high:
             mid = (low + high) // 2
-
-            is_possible = self.cal(nums, mid, m)
-
-            if is_possible:
+            
+            if self.is_possible(nums, mid, m):
                 high = mid - 1
-
             else:
                 low = mid + 1
-
         return low
     
-    def cal(self, nums, mid, m):
-        students = 1
-        sum = 0
+    def is_possible(self, nums, mid, m):
+        count = 1
+        current_sum = 0
         for i in nums:
-            if sum + i > mid:
-                students += 1
-                sum = i
+            if i + current_sum > mid:
+                count += 1
+                current_sum = i
             else:
-                sum += i
-        return students <= m
+                current_sum += i
+        return count <= m
