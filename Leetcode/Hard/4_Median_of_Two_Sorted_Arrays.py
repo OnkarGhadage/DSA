741e3820224cf0d1bfe86f4f94ab95c1df38dde7
OnkarGhadage <onkaryg@gmail.com>
2026-03-06 23:41:21 +0530

    Add BS approach to the Leetcode problem 4

--- Leetcode/Hard/4_Median_of_Two_Sorted_Arrays.py
+++ Leetcode/Hard/4_Median_of_Two_Sorted_Arrays.py
@@ -18,3 +18,31 @@ class Solution:
             return (merged[n//2] + merged[n//2-1]) / 2
         else:
             return merged[n//2]
+
+### Binary Search Approach
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        low, high = 0, len(nums1)
+        n, m = len(nums1), len(nums2)
+        while low <= high:
+            cut1 = (low + high) // 2
+            cut2 = (n + m + 1) // 2 - cut1
+
+            l1 = float('-inf') if cut1 == 0 else nums1[cut1 - 1]
+            l2 = float('-inf') if cut2 == 0 else nums2[cut2 - 1]
+
+            r1 = float('inf') if cut1 == n else nums1[cut1]
+            r2 = float('inf') if cut2 == m else nums2[cut2]
+
+            if l1 <= r2 and l2 <= r1:
+                if (n + m) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                else:
+                    return max(l1, l2)
+            elif l1 > r2:
+                high = cut1 - 1
+            else:
+                low = cut1 + 1
