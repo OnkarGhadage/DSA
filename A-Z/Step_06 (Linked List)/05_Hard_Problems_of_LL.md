183d1e3c9db862d2e9f09930e5308ac1931b31fe
OnkarGhadage <onkaryg@gmail.com>
2026-04-03 17:50:34 +0530

    Add solution of problem '25. Reverse Nodes in k-Group'

---
+++ A-Z/Step_06 (Linked List)/05_Hard_Problems_of_LL.md
@@ -0,0 +1,5 @@
+# Hard Problems of LL
+
+### ⭐ 1. Reverse LL in group of given size K | 25. Reverse Nodes in k-Group
+📘 **TUF** - [Problem statement](https://takeuforward.org/plus/dsa/problems/reverse-ll-in-group-of-given-size-k?source=strivers-a2z-dsa-track) → [**Solution**](../Problem/Hard/18_Reverse_LL_in_group_of_given_size_K.py)  
+📙 **Leetcode** - [Problem statement](https://leetcode.com/problems/reverse-nodes-in-k-group/) → [**Solution**](../../Leetcode/Hard/25_Reverse_Nodes_in_k-Group.py)  
---
+++ Leetcode/Hard/25_Reverse_Nodes_in_k-Group.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        grpPrev = dummy
+
+        while True:
+            Kth = self.getKth(grpPrev, k)
+            if not Kth:
+                break
+            
+            grpNext = Kth.next
+
+            prev = grpNext
+            curr = grpPrev.next
+            while curr != grpNext:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            
+            temp = grpPrev.next
+            grpPrev.next = Kth
+            grpPrev = temp
+        return dummy.next
+        
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
